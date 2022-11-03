5a4827d5c1e70ca8e7554f2494cbd9252d64e00d
Marek <mmarchewka@student.agh.edu.pl>
2022-11-03 09:16:21 -0700

    use env variables

--- WebApplicationLab2/Program.cs
+++ WebApplicationLab2/Program.cs
@@ -1,6 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello from CI/CD 03.11!");
-
+var configuration = (IConfiguration)app.Services.GetService(typeof(IConfiguration))!;
+app.MapGet("/", () => $"Hello World! 03.11 --Value: {configuration.GetSection("test").Value}");
 app.Run();
